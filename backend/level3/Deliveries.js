36e9b86177f24bf46204d73395eb0803a5ddfc81
elinlarsen <elin_larsen1@hotmail.fr>
2019-08-26 20:05:53 +0200

    fixing typos and formatting with prettier

--- backend/level3/Deliveries.js
+++ backend/level3/Deliveries.js
@@ -1,25 +1,25 @@
-'use strict';
-const Delivery=require('./Delivery.js')
+"use strict";
+const Delivery = require("./Delivery.js");
 
+function getDeliveries(inputData) {
+  this.sortByPackageId = function(deliveries) {
+    return deliveries.sort((a, b) => a.package_id - b.package_id);
+  };
 
-function getDeliveries(inputData){
+  let deliveries = [];
 
-    this.sortByPackageId =function (deliveries){
-        return deliveries.sort( (a,b) => a.package_id -b.package_id )
-    }
-
-    let deliveries=[]
-
-    inputData.carriers.forEach( (carrier)=>{
-        inputData.packages.forEach( (pack) => {
-            if(carrier.code === pack.carrier ){
-                let delivery= new Delivery(carrier, pack, inputData.country_distance).getDelivery()
-                deliveries.push(delivery) 
-            }
-        })
-    })
-
-    return {"deliveries" : this.sortByPackageId(deliveries)}
-   
+  inputData.carriers.forEach(carrier => {
+    inputData.packages.forEach(pack => {
+      if (carrier.code === pack.carrier) {
+        let delivery = new Delivery(
+          carrier,
+          pack,
+          inputData.country_distance
+        ).getDelivery();
+        deliveries.push(delivery);
+      }
+    });
+  });
+  return { deliveries: this.sortByPackageId(deliveries) };
 }
-module.exports=getDeliveries
+module.exports = getDeliveries;
